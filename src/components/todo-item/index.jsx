e010b5ebfb27c6b833520eb4537f13affa95f66e
mohammad-makhamreh <mohammadmakhamrah@gmail.com>
2025-02-23 21:21:38 +0200

    first commit

---
+++ src/components/todo-item/index.jsx
@@ -0,0 +1,30 @@
+import { Button, Card, CardActions, CardContent, Typography } from "@mui/material";
+
+
+
+function TodoItem({ todo, fetchDetailsOfCurrentTodo}) {
+
+    return(
+        <Card sx={{maxWidth:350, display:"flex", flexDirection: "column", justifyContent:"space-between"}}>
+            <CardContent>
+                <Typography varient="h5" color={'text.secondary'}>{todo?.todo}</Typography>
+            </CardContent>
+            <CardActions>
+                <Button
+                onClick={()=> fetchDetailsOfCurrentTodo(todo?.id)}
+                sx={{
+                    backgroundColor:'black',
+                    color: 'white',
+                    opacity: '0.75',
+                    '&:hover': {
+                        backgroundColor:'black',
+                        color: 'white',
+                        opacity: '1',
+                    }
+                }}>Details</Button>
+            </CardActions>
+        </Card>
+    )
+}
+
+export default TodoItem;
